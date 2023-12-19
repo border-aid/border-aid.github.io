54e072399543942d2c5fac2ee198880dd87ffe7e
Allison-Turner <aturner@ucsd.edu>
2023-12-18 19:19:37 -0800

    got image modals onclick to work!

---
+++ scripts.js
@@ -0,0 +1,24 @@
+
+function show_modal_img(clicked_id)
+{
+// Get the modal
+var modal = document.getElementById("img-modal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById(clicked_id);
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+}
